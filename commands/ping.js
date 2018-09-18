35c19d0ef435fce579ce10a134f323f0d6f2bce8
JAX3 <thecore95x@gmail.com>
2018-09-18 21:58:16 +0100

    rewrote the ping command
    Uses embeds cus im fancy

--- commands/ping.js
+++ commands/ping.js
@@ -1,12 +1,17 @@
 
-const Discord = require("discord.js");
-
+const Discord = require("discord.js");//require discord.js from the module folder
+const config = require("../serverconfig.json")//requireing the serverconfig file
 module.exports.run = async (client, message, args) => {
-
-		  message.channel.send(`Pong! ⏱: ${Math.round(client.ping)}ms.`);
-		 
+  var time = 5000;//time 5 seconds 
+      let pingEmbed = new Discord.RichEmbed()
+      .setTitle("Pong")
+      .setColor(config.red)//embed colour.
+      .setDescription(`Pong! ⏱: ${Math.round(client.ping)}ms`)//displaying the client's ping ms
+      .setThumbnail('⏱')
+      
+      message.channel.send(pingEmbed).then(m => m.delete(time));//message send/delete
 }
 
 module.exports.help = {
-  name:"ping"
+  name:"ping"//command name 
 }
